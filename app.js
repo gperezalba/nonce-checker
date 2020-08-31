cd5158ce66d4df6cab64670ccab36fd9aa04b758
gperezalba <gperezalba94@gmail.com>
2020-08-31 17:18:46 +0200

    update

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const URL = "https://connect.pichain.io";
-const MANUEL1 = "0x95b9bbC3A8c0dE0695d727c3B79b44f3721232c9";
+const MANUEL1 = "";
+const MANUEL2 = "";
+const MANUEL3 = "";
+
+const JOHAN1 = "";
+const JOHAN2 = "";
+const JOHAN3 = "";
+
+const GUILLE1 = "0xf93e2E6B8018D4c3aaf455cfE2b20463E10162d0";
+const GUILLE2 = "0xA246C82ddd6D13c8C2cBF7A6e2838A9e65c8DA19";
+const GUILLE3 = "0xa9C4b0EB0Ede565539B6E80cd14096B3f276F79A";
 
 async function updateInfo() {
-	document.getElementById(MANUEL1).innerHTML = await getTransactionCount(MANUEL1)
+	document.getElementById(GUILLE1).innerHTML = await getTransactionCount(GUILLE1);
+	document.getElementById(GUILLE2).innerHTML = await getTransactionCount(GUILLE2);
+	document.getElementById(GUILLE3).innerHTML = await getTransactionCount(GUILLE3);
 }
 
 async function getTransactionCount(wallet) {
